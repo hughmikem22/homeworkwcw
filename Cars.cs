dca9936c5222f8ec8077f0242839fe770d72bc87
hughmikem22 <50379908+hughmikem22@users.noreply.github.com>
2019-05-30 15:21:30 -0500

    Add files via upload

---
+++ Cars.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs1
+{
+    class Cars
+    {
+        public string make { set; get; }
+        public string model { set; get; }
+        public string color { set; get; }
+        public int hp { set; get; }
+        public double engine { set; get; }
+        public int year { set; get; }
+
+        public Cars(string aModel, string aColor, string aMake, int aYear, int aHP, double aEngine)
+        {
+            model = aModel;
+            make = aMake;
+            year = aYear;
+            hp = aHP;
+            engine = aEngine;
+            color = aColor;
+
+        }
+        public  bool IsFast()
+        {
+            if ( hp >= 240)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+
+        }
+        public bool IsOld()
+        {
+            if (year <= 2000)
+            {
+                return true;
+                
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public void Classic()
+        {
+            if (make == "BMW")
+            {
+                Console.WriteLine("This is a very nice car tis indeed a classic");
+            }
+            else
+            {
+                Console.WriteLine("Maybe you should consider and upgrade son, you are riding in a bucket");
+            }
+            
+        }
+    }
+}
---
+++ Foxtail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Programs1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars car1 = new Cars("Chevrolet", "Corvette", "Red", 1983, 345, 3.6 );
+            Cars car2 = new Cars("BMW", "830i", "Midnight Blue", 1992, 236, 2.8);
+            Cars car3 = new Cars("Mercedes Benz","Mclass","Maroon", 2018, 255, 3.2);
+            Cars car4 = new Cars("Nissan", "altima" , "black", 2016, 243, 3.6);
+            Cars car5 = new Cars("BMW", "328i" , "blue", 2009, 267, 3.9);
+            Cars car6 = new Cars("Ford", "bronco 2" , "white", 1988, 212, 4.6);
+            Cars car7 = new Cars("Chevrolet", "Malibu" , "blue", 1999, 180, 2.7);
+
+
+
+            Console.WriteLine(car2);
+            
+
+
+        }
+    }
+}

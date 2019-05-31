fc8f7c28e6443ea4539c32e9be1687c929f6c46c
hughmikem22 <50379908+hughmikem22@users.noreply.github.com>
2019-05-31 15:24:52 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,100 +1,15 @@
 ﻿using System;
 
-namespace HW_5_28_2019
+namespace La_Frutas
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Fruits FRT1 = new Fruits("Banana", "Yellow","Like a Sausage",4.8);
 
-            Console.WriteLine("Hello World");
-
-            Console.WriteLine("So, you want to be a bad ass huh");
-            Console.WriteLine("So how bad are ya? 1. Super Bad ass, 2. Very Very Billy Bad ass, 3. Mega Uzumaki Bad ASS");
-
-            string BadASS = Console.ReadLine();
-
-            switch (BadASS)
-            {
-                case "1":
-                    Console.WriteLine(" so you must be Super Bad ass soooo... ");
-                    congrats();
-                    break;
-                case "2":
-                    Console.WriteLine(" so you must be Very Very Billy Bad ass");
-                    gotit();
-                    break;
-                case "3":
-                    Console.WriteLine(" so you must be a Mega Uzumaki Bad ASS so your sooooo MR.");
-                    childsplay();
-                    break;
-            }
-
-
+            FRT1.choice();
         }
-        private static void childsplay ()
-        {
-            Console.WriteLine(" if you really like candy then let me know how much you like it");
-            string[] sweetsList = { "ButterFinger", "Kit-Kat", "Snickers", "Hersheys" };
 
-            
-            int yums = 0;
-
-            for(int i = 0; i < sweetsList.Length; i++)
-            {
-                Console.WriteLine("Do you like {0}?", sweetsList[i]);
-                Console.WriteLine("yes or no");
-
-                string si = Console.ReadLine().ToLower();
-                if (si == "yes")
-                {
-                    yums++;
-                }
-                else
-                {
-                    Console.WriteLine("yeah that stuff is trash");
-                }
-
-                Console.WriteLine(yums);
-
-            }
-        }
-        private static void congrats()
-        {
-            string[] bestWrestlers = { "Ric Flair", "Dusty Rhodes", "Sting", "Rowdy roddy Piper" };
-
-            
-            int WCW = 0;
-
-            for (int i = 0; i < bestWrestlers.Length; i++)
-            {
-                Console.WriteLine("Do you like {0}?", bestWrestlers[i]);
-                Console.WriteLine("Dope or Nope");
-
-                string a = Console.ReadLine().ToLower();
-                if (a == "Dope")
-                {
-                    WCW++;
-                }
-                else
-                {
-                    Console.WriteLine("trash");
-                }
-                Console.WriteLine(WCW);
-
-
-            }
-        } public static void gotit()
-        {
-            int multiply = 10;
-
-            string Multi(int num1)
-            {
-                return $"String has value:{num1 * multiply++}";
-            }
-            Console.WriteLine(Multi(7));
-            Console.WriteLine(Multi(6));
-        }
     }
-    
 }
---
+++ fruits.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace La_Frutas
+{
+    class Fruits
+
+    {
+        public string _color { set; get; }
+        public string _shape  { set; get; }
+        public double _weight  { set; get; }
+
+        public Fruits(string name, string color, string shape, double weight)
+        {
+            _color = color;
+            _shape = shape;
+            _weight = weight;
+
+
+        }
+        public void choice()
+        {
+            Console.WriteLine("I only eat fruit that  have the color of{0}", _color);
+        }
+        public class Berries : Fruits
+        {
+           public string _name { get; set; }
+
+            public Berries(string name, string color, string shape, double weight) : base(name, color, shape, weight)
+            {
+                
+                _name = name; 
+            }
+
+            public void bite()
+            {
+                Console.WriteLine("mmm this {0} is quite tasty?", _name);
+
+            }
+
+        }
+        public void pick()
+        {
+            Console.WriteLine("hmm which one do i want it has to look like a {0}?", _shape);
+
+        }
+        public void requestWeight()
+        {
+            Console.WriteLine(" how much does that weigh? ", _weight);
+
+        }
+       
+    }
+    
+}
